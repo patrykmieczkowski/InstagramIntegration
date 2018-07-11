2cacf05b2151c31f9ac3d13b9f9dae4b7d5d3d0c
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-07-11 19:25:29 +0200

    handling redirect flow

--- app/src/main/java/com/mieczkowskidev/instagramintegration/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/instagramintegration/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mieczkowskidev.instagramintegration
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val clientId = ""
+    private val redirectUri = "sociallogin://redirect"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_web_view.loadUrl(
+                "https://api.instagram.com/oauth/authorize/?client_id=$clientId&redirect_uri=$redirectUri&response_type=token")
     }
 }
